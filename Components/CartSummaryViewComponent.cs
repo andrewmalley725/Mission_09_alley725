41af704d434ffe4c61c213a289ec08392f6117ae
andrewmalley725 <andrew.alley99@gmail.com>
2023-03-15 22:21:26 -0600

    added cart summary

---
+++ Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Mission_09_alley725.Models;
+
+namespace Mission_09_alley725.Components
+{
+	public class CartSummaryViewComponent : ViewComponent
+	{
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
